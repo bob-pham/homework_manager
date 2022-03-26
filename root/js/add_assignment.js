b3866af15578cf1fb94f01a6302799fac208959b
Bob Pham <bobbaopham@gmail.com>
2022-03-26 14:06:44 -0700

    added more front-end to add assignment

---
+++ root/js/add_assignment.js
@@ -0,0 +1,13 @@
+
+
+function makeVisible(element) {
+    element.style.display = "flex";
+}
+
+function makeInvisible(element) {
+    element.style.display = "None";
+}
+
+function saveAssessment() {
+    // makeInvisible(createAssessment);
+}

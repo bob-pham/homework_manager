81dfc5f530f8a3655c323dfd909923a9d0138624
Bob Pham <bobbaopham@gmail.com>
2022-03-26 00:01:02 -0700

    added more stubs

---
+++ root/js/add_class.js
@@ -0,0 +1,29 @@
+const createAssessment = document.querySelector("#create_assessment");
+
+function makeVisible(element) {
+    element.style.display = "flex";
+}
+
+function makeInvisible(element) {
+    element.style.display = "None";
+}
+
+function saveAssessment() {
+    makeInvisible(createAssessment);
+}
+
+function createAssessment() {
+    //TODO
+}
+
+function updateSyllabus() {
+    //TODO
+}
+
+function calculateValidSyllabus() {
+    //TODO
+}
+
+function saveSyllabus() {
+    //TODO
+}
